f064e2c72f0c4481520f89297302c03fcf1107a4
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-07-26 23:22:37 +0900

    감소하는 수

--- 알고리즘 다지기 기초 백준 문제 추천/06_감소하는수.py
+++ 알고리즘 다지기 기초 백준 문제 추천/06_감소하는수.py
@@ -13,27 +13,35 @@ def solution():
     if target < 10:
         return target
     else:
-        number = 9
-        cnt = 10
+        cnt = 9
+        number = 10
 
-    while cnt <= target:
-        # print("=========\n", cnt, number)
-        str_num = str(number)
-        cur_num = int(str_num[0])
+    while True:
         is_decreasing = True
-        for s in str_num[1:]:
-            # print("s:", s)
-            if cur_num <= int(s):
+        str_num = str(number)
+        for i in range(1, len(str_num)):
+            if int(str_num[i]) < int(str_num[i - 1]):
+                continue
+            else:
+                start = str_num[: i - 1]
+                mid = str(int(str_num[i - 1]) + 1)
+                end = "0" + str_num[i + 1 :]
+                number = int(start + mid + end)
                 is_decreasing = False
                 break
-            cur_num = int(s)
+
         if is_decreasing:
             cnt += 1
-        number += 1
+            if cnt == target:
+                return number
+            number += 1
 
-    return number
+    return -1
 
 
 target = int(input())
-result = solution()
-print(result)
+if target >= 1023:  # 1022번째 => 9876543210(N은 1,000,000보다 작거나 같은 자연수 또는 0이다.)
+    print(-1)
+else:
+    result = solution()
+    print(result)
