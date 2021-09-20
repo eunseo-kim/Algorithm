25ff0125d6c8fbd8c0c7f96ee3659cf3d9175a8e
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-09-20 23:15:00 +0900

    Create A_나머지.py

---
+++ Koreatech Judge/A_나머지.py
@@ -0,0 +1,8 @@
+# 언어 : Python
+# 날짜 : 2021.09.20
+# 문제 : KOREATECH JUDGE > 나머지 (2020년도 A번 문제)
+# ==============================================================
+
+for _ in range(int(input())):
+    A, B = map(int, input().split())
+    print(A % B)
