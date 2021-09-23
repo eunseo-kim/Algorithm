1961ceb6694f632d468179d1a64a57a0026ebc19
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-09-23 16:02:09 +0900

    Create 15_N과M.py

---
+++ BOJ/class4/15_N과M.py
@@ -0,0 +1,19 @@
+# 언어 : Python
+# 날짜 : 2021.09.23
+# 문제 : BOJ > 파티 (https://www.acmicpc.net/problem/15650)
+# 티어 : 실버 3
+# ==========================================================
+
+
+def dfs(n, path):
+    if len(path) == M:
+        print(" ".join(map(str, path)))
+        return
+
+    for i in range(n + 1, N + 1):
+        dfs(i, path + [i])
+
+
+N, M = map(int, input().split())
+for i in range(1,N+1):
+    dfs(i, [i])
