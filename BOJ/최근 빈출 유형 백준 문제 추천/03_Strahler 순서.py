d8aaa53d105decd21d9cf16598801568deecf932
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2022-01-10 17:17:22 +0900

    Strahler 순서

--- BOJ/최근 빈출 유형 백준 문제 추천/03_Strahler 순서.py
+++ BOJ/최근 빈출 유형 백준 문제 추천/03_Strahler 순서.py
@@ -2,91 +2,68 @@
 # 날짜 : 2022.1.10
 # 문제 : BOJ > Strahler 순서(https://www.acmicpc.net/problem/9470)
 # 티어 : 골드 3
-# ================================================================
+# - 위상정렬 문제 (위상정렬 참고 blog: https://freedeveloper.tistory.com/390)
+# - 파이썬 알고리즘 인터뷰 p.417 (단계별 리프 노드 제거) 참고
+# =========================================================================
 
 from collections import defaultdict
 from collections import Counter
 
 
-def Strahler():
-    # [1] 강의 근원 노드 찾기
-    is_root = [True for _ in range(M + 1)]
-    is_root[0] = False  # 노드 번호는 1부터 시작
+def get_leaves(graph, visited):
+    leaves = []
+    is_leave = [True for _ in range(M)]
     for key in graph:
-        for value in graph[key]:
-            is_root[value] = False
+        for node in graph[key]:
+            is_leave[node] = False
 
-    strahler = [[0] for _ in range(M + 1)]  # [들어오는 모든 강]
-    roots = []
-    for i, root in enumerate(is_root):
-        if root:
-            roots.append([1, i + 1])
-            strahler[i] = [1]
+    for leaf, is_leaf in enumerate(is_leave):
+        if is_leaf and not visited[leaf]:
+            leaves.append(leaf)
 
-    visited = [False for _ in range(M + 1)]
-
-    # [2] 강의 근원 노드로부터 차례대로 들어오는 강 계산
-    queue = roots[:]
-    while queue:
-        num, node = queue.pop(0)
-        if not visited[node]:
-            visited[node] = True
-
-            # 현재 node의 순서 계산하기
-            strahler[node].sort()
-            counter = Counter(strahler[node])
-            num = max(strahler[node])
-            if counter[strahler[node][-1]] >= 2:
-                num += 1
-            strahler[node] = [num]
-
-            # 다음 노드 탐색
-            next_nodes = graph[node]
-            for next in next_nodes:
-                strahler[next].append(num)
-
-            # 탐색 완료된 근원 노드 삭제하기
-            graph[node] = []
-            for key in graph:
-                for value in graph[key]:
-                    if value == node:
-                        graph[key].remove(node)
-
-            print("graph:", graph)
-
-            is_root = [True for _ in range(M + 1)]
-            is_root[0] = False  # 노드 번호는 1부터 시작
-            for key in graph:
-                for value in graph[key]:
-                    is_root[value] = False
-
-            print("IS_ROOT:", is_root)
-            for i, root in enumerate(is_root):
-                print(i, root)
-                if root and not visited[root]:
-                    queue.append([num, i + 1])
-
-            print(node, ">", strahler)
-            print("VISITED:", visited)
-            print("QUEUE:", queue)
-            print()
-
-    # [3] strahler 순서 계산하기
-    strahler[M].sort()
-    counter = Counter(strahler[M])
-    if counter[strahler[M][-1]] >= 2:
-        return strahler[M][-1] + 1
-    else:
-        return strahler[M][-1]
+    return leaves
 
 
+def Strahler():
+    visited = [False for _ in range(M)]
+
+    # 초기 리프 노드(근원 노드)들 추가
+    leaves = get_leaves(graph, visited)
+    strahler = [[] for _ in range(M)]
+    for leaf in leaves:
+        strahler[leaf] = [1]
+
+    while leaves:
+        for leaf in leaves:
+            if not visited[leaf]:
+                visited[leaf] = True
+
+                # 현재 탐색하는 노드의 순서번호 결정하기
+                seq_num = max(strahler[leaf])
+                counter = Counter(strahler[leaf])
+                if counter[seq_num] >= 2:
+                    seq_num += 1
+                strahler[leaf] = [seq_num]
+
+                # 이웃 노드들에게 현재 탐색 노드의 순서번호 전달하기
+                neighbors = graph[leaf]
+                for neighbor in neighbors:
+                    strahler[neighbor].append(seq_num)
+                graph[leaf] = []
+
+        leaves = get_leaves(graph, visited)
+
+    return strahler[M - 1][0]
+
+
+# 입력 및 실행
 T = int(input())
 for _ in range(T):
     graph = defaultdict(list)
     K, M, P = map(int, input().split())
     for _ in range(P):
         A, B = map(int, input().split())
-        graph[A].append(B)
+        graph[A - 1].append(B - 1)
 
     result = Strahler()
     print(K, result)
