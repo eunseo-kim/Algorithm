bffeb0a1d083243cf17288d85084ebb6a4e99486
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2022-02-17 23:28:30 +0900

    dp / 1, 2, 3 더하기

---
+++ BOJ/코딩테스트 대비 문제집 with Baekjoon/DP/03_1, 2, 3 더하기.py
@@ -0,0 +1,28 @@
+# 언어 : Python
+# 날짜 : 2022.2.17
+# 문제 : BOJ > 1, 2, 3 더하기 (https://www.acmicpc.net/problem/9095)
+# 티어 : 실버 3
+# =========================================================================
+
+
+def solution(dp, n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+
+    dp[0] = 1
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+    return dp[N]
+
+
+for _ in range(int(input())):
+    N = int(input())
+    dp = [0 for _ in range(N + 1)]
+    result = solution(dp, N)
+    print(result)
