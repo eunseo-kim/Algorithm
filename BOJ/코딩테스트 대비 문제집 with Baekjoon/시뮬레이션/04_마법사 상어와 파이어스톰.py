8b5ce728534f304784e881e68fd99ec48d7f38a2
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2022-03-06 22:20:00 +0900

    시뮬레이션 / 마법사 상어와 파이어스톰🔥

--- BOJ/코딩테스트 대비 문제집 with Baekjoon/시뮬레이션/04_마법사 상어와 파이어스톰.py
+++ BOJ/코딩테스트 대비 문제집 with Baekjoon/시뮬레이션/04_마법사 상어와 파이어스톰.py
@@ -5,37 +5,71 @@
 # =============================================================================
 
 
-def rotate(l, row, col):
-    global changed_graph
-    N = l
+def rotate(gap, row, col):
+    global graph
 
-    while N > 0:
-        for r in range(row, row + l):
-            for c in range(col, col + l):
-                changed_graph[r][c] = graph[c][N - 1 - r]
+    # tmp에 복사하기
+    tmp = []
+    for r in range(row, row + gap):
+        tmp.append(graph[r][col : col + gap])
 
-        N -= 2
-
-    for g in changed_graph:
-        print(g)
-    print("===")
+    # tmp를 회전해서 graph를 갱신
+    for r in range(gap):
+        for c in range(gap):
+            graph[row + r][col + c] = tmp[gap - c - 1][r]
 
 
 def solution(l):
-    print("check1:", l)
     global graph
 
     # rotate
     for row in range(1, n + 1, l):
         for col in range(1, n + 1, l):
             rotate(l, row, col)
-    graph = changed_graph
+
+    # 얼음 3개와 닿아있지 않은 곳
+    moves = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    delete = []
+    for r in range(1, n + 1):
+        for c in range(1, n + 1):
+            if graph[r][c] != 0:
+                count = 0
+                for m_row, m_col in moves:
+                    new_r, new_c = r + m_row, c + m_col
+                    if graph[new_r][new_c] == 0:
+                        count += 1
+                if count > 1:
+                    delete.append([r, c])
+
+    # 얼음의 양 1 줄기
+    for r, c in delete:
+        graph[r][c] -= 1
+
+
+def dfs(row, col):
+    global visited
+    queue = [[row, col]]
+    size = 0
+
+    moves = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+    while queue:
+        curr = queue.pop()
+        if not visited[curr[0]][curr[1]] and graph[curr[0]][curr[1]] != 0:
+            visited[curr[0]][curr[1]] = True
+            size += 1
+
+            for m_r, m_c in moves:
+                new_r, new_c = curr[0] + m_r, curr[1] + m_c
+                if not visited[new_r][new_c] and graph[new_r][new_c] != 0:
+                    queue.append([new_r, new_c])
+
+    return size
 
 
 N, Q = map(int, input().split())
 n = pow(2, N)
 graph = [[0 for _ in range(n + 2)]]
-changed_graph = [[0 for _ in range(n + 2)] for _ in range(n + 2)]
 for _ in range(n):
     graph.append([0] + list(map(int, input().split())) + [0])
 graph.append([0 for _ in range(n + 2)])
@@ -44,3 +78,19 @@ L_list = list(map(int, input().split()))
 for i in range(Q):
     L = pow(2, L_list[i])
     solution(L)
+
+# 남아있는 얼음의 합 구하기
+result_sum = 0
+for r in range(n + 1):
+    for c in range(n + 1):
+        result_sum += graph[r][c]
+
+# dfs로 가장 큰 얼음 덩어리 구하기
+max_size = 0
+visited = [[False for _ in range(n + 2)] for _ in range(n + 2)]
+for r in range(1, n + 1):
+    for c in range(1, n + 1):
+        max_size = max(max_size, dfs(r, c))
+
+print(result_sum)
+print(max_size)
