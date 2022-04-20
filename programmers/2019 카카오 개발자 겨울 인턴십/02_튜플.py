23401d9bc0637b9df7f54824d228c53629e0dba1
Eunseo Kim <kim.eunseo@kakao.com>
2022-04-20 21:38:30 +0900

    02_튜플 > 수정

--- programmers/2019 카카오 개발자 겨울 인턴십/02_튜플.py
+++ programmers/2019 카카오 개발자 겨울 인턴십/02_튜플.py
@@ -1,41 +1,19 @@
-from unicodedata import decimal
-
-# {2},{2,1},{2,1,3},{2,1,3,4}
-# {20,111},{111}
-
-
 def solution(s):
-    idx = 0
-    s = s[1:-1]
+    s = s[2:-2]
+    s = s.split("},{")
+
+    nums = set()
+    answer = []
     group = []
-    length = len(s)
-    while idx < length:
-        if s[idx] == ",":
-            idx += 1
-        if s[idx] == "{":
-            current_group = []
-            idx += 1
-            number = 0
-            while s[idx] != "}":
-                if s[idx] == ",":
-                    idx += 1
-                    current_group.append(number)
-                    number = 0
-                    continue
-                number = 10 * number + int(s[idx])
-                idx += 1
-            current_group.append(number)
-            group.append(current_group)
-            idx += 1
+    for string in s:
+        group.append(string.split(","))
 
     group.sort(key=len)
-    numbers = set()
-    answer = []
-
-    for part in group:
-        for num in part:
-            if num not in numbers:
-                numbers.add(num)
+    for letters in group:
+        for letter in letters:
+            num = int(letter)
+            if num not in nums:
+                nums.add(num)
                 answer.append(num)
                 break
 
