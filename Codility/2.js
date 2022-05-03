2e58be405c8a6e8f73b69fae772f3bb038531df2
Eunseo Kim <kim.eunseo@kakao.com>
2022-05-03 23:11:16 +0900

    Codility 1, 2, 3

---
+++ Codility/2.js
@@ -0,0 +1,28 @@
+
+function solution(S) {
+    let prevLetter = S[0];    // N은 1 ~ 200000이므로 S[0]이 존재함
+    let index = 1;            // index of S
+    let result = prevLetter;  // 첫 글자는 포함하고 시작함
+    let continuousLength = 1; // 연속되는 길이 (첫 글자를 포함하므로 초깃값은 1)
+
+    const max_length = S.length;
+
+    while (index < max_length) {
+        const currentLetter = S[index];
+
+        // 현재 글자의 연속되는 길이가 3보다 크면 result에 넣지 않음
+        if (currentLetter === prevLetter) {
+            continuousLength += 1;
+            if (continuousLength < 3) {
+                result += currentLetter;
+            }
+        } else { // 연속이 끊기는 부분 (continuousLength, prevLetter을 새로 갱신함)
+            result += currentLetter;
+            continuousLength = 1;
+            prevLetter = S[index];
+        }
+        index += 1;
+    }
+
+    return result;
+}
---
+++ Codility/3.js
@@ -0,0 +1,19 @@
+function solution(S) {
+    const maxLength = S.length;
+
+    let cut = 1; // cut의 초깃값은 1
+    let letterSet = new Set();
+
+    for (let i=0; i<maxLength; i++) {
+        const currentLetter = S[i];
+
+        // 이전까지의 letterSet에 중복되는 letter이 있으면 cut을 1 늘린다.
+        if (letterSet.has(currentLetter)) {
+            cut += 1;
+            letterSet.clear(); // 그리고 letterSet을 다시 초기화시킨다.
+        }
+        letterSet.add(currentLetter);
+    }
+
+    return cut
+}
